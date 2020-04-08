26faf16953bb0414e8b660ad187a5ef3d30d3417
yuhiusui <usui.hanshin@gmail.com>
2020-04-08 23:32:03 +0900

    [add]エラー

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'home#top'
   get 'home/about' =>'home#about'
-  resources :users, only: [:show, :edit, :update, :index]
+  resources :users
   resources :books
 end
